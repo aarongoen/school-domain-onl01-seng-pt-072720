34b5415516f318c1a39aebae1f9d31b7cd413520
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-12 16:50:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,20 +19,14 @@ def initialize(school)
     end 
     @roster[grade]
   end 
-    
-  #   student_names = add_student[:grade_level]
-  #   add_student[grade] = []
-  #   add_student[grade] = ["new_value_for_value_array"]
-  #   add_student => {add_student=>["new_value_for_value_array"]}
-  # end 
   
   def grade(grade)
-    @roster[:grade]
+    @roster[grade]
   end 
   
-  def sort 
-    add_student.each do |grade, name|
-      return #{grade} => #{name}
+  def sort(student, grade)
+    @roster[grade].map do |grade, name|
+      #{grade} #{name}
     end 
   end 
 end 
