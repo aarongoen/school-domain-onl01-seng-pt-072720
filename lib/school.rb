70749109e59ebbb7100a955ccb128cb752780750
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-12 15:50:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 # code here!
 class School
-  attr_accessor :roster
+  attr_accessor :roster, :grade, :name 
   
   attr_reader
   
@@ -10,7 +10,7 @@ class School
 
   roster = {}
   
-  def add_student(name, grade)
+  def add_student(grade, name)
     add_student[name] = []
     add_student[name] = "new_value_for_value_array" 
     add_student => {add_student=>["new_value_for_value_array"]}
@@ -20,4 +20,13 @@ class School
     add_student => {add_student=>["new_value_for_value_array"]}
   end 
   
+  def grade(grade)
+    add_student[grade]=>name 
+  end 
+  
+  def sort 
+    add_student.each do |grade, name|
+      return #{grade} => #{name}
+    end 
+  end 
 end 
