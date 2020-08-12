42173c04da0840f9fb28cbe951e60fed615b577f
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-12 15:43:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  attr_accessor :roster
+  
+  attr_reader
+  
+  initialize name=(name)
+    @name = name 
+  end 
+
+  roster = {}
+  
+  def add_student(name, grade)
+    add_student[name] = []
+    add_student[name] = "new_value_for_value_array" 
+    add_student => {add_student=>["new_value_for_value_array"]}
+    
+    add_student[grade] = []
+    add_student[grade] = ["new_value_for_value_array"]
+    add_student => {add_student=>["new_value_for_value_array"]}
+  end 
+  
+end 
