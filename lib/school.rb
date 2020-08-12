9b790e0ba408f3a80fcd8b46d1d66d5a926a4668
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-12 17:00:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,11 +18,11 @@ def initialize(school)
     end 
   end 
   
-#   def grade(grade)
-#     @roster[grade]
-#   end 
+  def grade(grade)
+    @roster[grade]
+  end 
   
-#   def sort(grade)
-#     @roster[grade].sort 
-#   end 
-# end 
+  def sort(grade)
+    @roster[grade].sort 
+  end 
+end 
