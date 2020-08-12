6a195f7761ad3f056d0bf8d1d0043e761bc4e593
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-12 16:12:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,10 @@
   def add_student(grade, name)
     add_student[grade] = []
     add_student[grade] << "name" 
-    add_student => {grade=>["name"]}
+    add_student
+    end 
+    
+    => {grade=>["name"]}
     
     # student_names = add_student[:grade_level]
     # add_student[grade] = []
