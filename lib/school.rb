42c3a2d639fe3ae4b3582f7f9f98138686af9481
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-12 16:06:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,32 @@
 # code here!
 class School
+  attr_reader :school 
   attr_accessor :roster, :grade, :name 
   
-  attr_reader
-  
-  initialize name=(name)
-    @name = name 
+  initialize school=(school)
+    @school = school 
   end 
 
   roster = {}
   
   def add_student(grade, name)
-    add_student[name] = []
-    add_student[name] = "new_name" 
-    add_student => {add_student=>["new_value_for_value_array"]}
-    
-    student_names = add_student[:grade_level]
     add_student[grade] = []
-    add_student[grade] = ["new_value_for_value_array"]
-    add_student => {add_student=>["new_value_for_value_array"]}
+    add_student[grade] << "new_name" 
+    add_student => {grade=>["new_name"]}
+    
+    # student_names = add_student[:grade_level]
+    # add_student[grade] = []
+    # add_student[grade] = ["new_value_for_value_array"]
+    # add_student => {add_student=>["new_value_for_value_array"]}
   end 
   
-  def grade(grade)
-    add_student[grade]=>name 
-  end 
+  # def grade(grade)
+  #   add_student[grade]=>name 
+  # end 
   
-  def sort 
-    add_student.each do |grade, name|
-      return #{grade} => #{name}
-    end 
-  end 
+  # def sort 
+  #   add_student.each do |grade, name|
+  #     return #{grade} => #{name}
+  #   end 
+  # end 
 end 
